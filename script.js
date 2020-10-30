ef8f9876319fd6567550af96660f9bb845b7317c
unknown <subham.hero01@gmail.com>
2020-10-30 23:57:47 +0300

    made it work but need more work

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
-document.querySelector(".header__form").addEventListener("submit", (e) => {
-  e.preventDefault();
-});
-
-let map = L.map("map").setView([0, 0], 2);
+let map = L.map("map");
+map.setView([-33.8688, 151.209], 0);
+map.flyTo([-33.8688, 151.209], 15);
 L.tileLayer(
   "https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=KIgo2TvlDDD69kwl8OUo",
   {
     attribution: `<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>`,
   }
 ).addTo(map);
-let marker = L.marker([40.650002, -73.949997]).addTo(map);
-// fetch(
-//   `https://geo.ipify.org/api/v1?apiKey=at_X5XC3rVRJHHfJSpWjDql2Em5rxn1d&ipAddress=8.8.8.8`
-// )
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data);
-//     // map = L.map("map").setView([[data.location.lat, data.location.lng], 10]);
-//     let marker = L.marker([data.location.lat, data.location.lng]).addTo(map);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+let marker = L.marker([-33.8688, 151.209]).addTo(map);
+
+function updateMap(lat, lng) {
+  marker.setLatLng([lat, lng]);
+  map.flyTo([lat, lng], 15);
+}
+
+const form = document.querySelector(".header__form");
+const input = document.querySelector(".header__form__input");
+
+function updateInfo(title, info) {
+  let qs = "." + title + " " + ".panel__detail";
+  console.log(qs);
+  document.querySelector(qs).textContent = info;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let value = input.value;
+  let query;
+  if (value.trim() === "") {
+    return;
+  }
+  if (typeof value[0] === "number") {
+    query = "ipAddress";
+  }
+  if (typeof value[0] === "string") {
+    query = "domain";
+  }
+  fetch(
+    `https://geo.ipify.org/api/v1?apiKey=at_X5XC3rVRJHHfJSpWjDql2Em5rxn1d&${query}=${value}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.code) {
+        document.querySelector(".header__form__error").innerHTML =
+          data.messages;
+      }
+
+      updateMap(data.location.lat, data.location.lng);
+      updateInfo("ip", data.ip);
+      updateInfo(
+        "location",
+        `${data.location.region}, ${data.location.country}, ${data.location.postalCode}`
+      );
+      updateInfo("timezone", data.location.timezone);
+      updateInfo("isp", data.isp);
+      input.value = "";
+      document.querySelector(".header__form__error").innerHTML = "";
+    });
+});
